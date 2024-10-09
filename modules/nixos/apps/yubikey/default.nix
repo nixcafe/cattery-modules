e7c5e335f2249929241267195b612585e917d56a
sobte <i@sobte.me>
2024-10-10 07:14:29 +0800

    feat: add yubico-piv-tool and yubikey-touch-detector

--- modules/nixos/apps/yubikey/default.nix
+++ modules/nixos/apps/yubikey/default.nix
@@ -11,14 +11,20 @@ in
 {
   options.${namespace}.apps.yubikey = {
     enable = lib.mkEnableOption "yubikey";
+    agent.enable = lib.mkEnableOption "yubikey agent";
+    touch-detector.enable = lib.mkEnableOption "yubikey touch detector" // {
+      default = true;
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    services.yubikey-agent.enable = true;
-
-    ${namespace}.home.extraOptions = {
-      home.packages = with pkgs; [ yubikey-manager-qt ];
-    };
+    environment.systemPackages = with pkgs; [
+      yubikey-manager-qt
+      yubikey-manager
+      yubico-piv-tool
+    ];
+    services.yubikey-agent.enable = cfg.agent.enable;
+    programs.yubikey-touch-detector.enable = cfg.agent.enable;
   };
 
 }
