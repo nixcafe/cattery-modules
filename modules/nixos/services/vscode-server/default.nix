2e606b40c24bd09d4f0fc015c64e17498dd51f71
sobte <i@sobte.me>
2024-11-13 04:34:43 +0800

    feat: add vscode-server, fcitx5 to impermanence

--- modules/nixos/services/vscode-server/default.nix
+++ modules/nixos/services/vscode-server/default.nix
@@ -16,12 +16,23 @@ in
       type = attrs;
       default = { };
     };
+    persistence = lib.mkEnableOption "add files and directories to impermanence" // {
+      default = true;
+    };
   };
 
   config = lib.mkIf cfg.enable {
     services.vscode-server = {
       enable = true;
     } // cfg.extraOptions;
+
+    ${namespace}.home.extraOptions = {
+      ${namespace}.system.impermanence = lib.mkIf cfg.persistence {
+        directories = [
+          ".vscode-server"
+        ];
+      };
+    };
   };
 
 }
