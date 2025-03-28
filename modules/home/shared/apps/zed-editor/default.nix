ed06fbf3a4ec51f42fbc19379b9df18011485fe6
sobte <i@sobte.me>
2025-03-28 16:59:24 +0800

    feat: add zed-editor configuration options and vscode profiles options

--- modules/home/shared/apps/zed-editor/default.nix
+++ modules/home/shared/apps/zed-editor/default.nix
@@ -1,17 +1,35 @@
 {
   config,
   lib,
+  pkgs,
   namespace,
   ...
 }:
 let
   inherit (lib) mkOption types;
+  inherit (config.${namespace}.user) settings;
+
+  jsonFormat = pkgs.formats.json { };
+  jsonType = jsonFormat.type;
 
   cfg = config.${namespace}.apps.zed-editor;
 in
 {
   options.${namespace}.apps.zed-editor = with types; {
     enable = lib.mkEnableOption "zed-editor";
+    userSettings = mkOption {
+      type = jsonType;
+      default = settings.zed-editor.userSettings or { };
+    };
+    userKeymaps = mkOption {
+      type = jsonType;
+      default = settings.zed-editor.userKeymaps or { };
+    };
+    extensions = mkOption {
+      type = listOf str;
+      default = settings.zed-editor.extensions or [ ];
+      description = "https://github.com/zed-industries/extensions/tree/main/extensions";
+    };
     installRemoteServer = lib.mkEnableOption ''
       Whether to symlink the Zed's remote server binary to the expected
       location. This allows remotely connecting to this system from a
@@ -22,19 +40,26 @@ in
       in the wiki.
     '';
     defaultEditor = lib.mkEnableOption "zed-editor to $EDITOR";
-    persistence = lib.mkEnableOption "add files and directories to impermanence" // {
-      default = true;
-    };
     extraOptions = mkOption {
       type = attrs;
       default = { };
+    };
+    persistence = lib.mkEnableOption "add files and directories to impermanence" // {
+      default = true;
     };
   };
 
   config = lib.mkIf cfg.enable {
     programs.zed-editor = {
-      inherit (cfg) enable installRemoteServer;
+      inherit (cfg)
+        enable
+        userSettings
+        userKeymaps
+        extensions
+        installRemoteServer
+        ;
     } // cfg.extraOptions;
+
     home.sessionVariables = lib.mkIf cfg.defaultEditor {
       EDITOR = "zeditor --new --wait";
       VISUAL = "$EDITOR";
