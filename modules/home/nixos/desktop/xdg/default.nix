d66696f63624551e1eda8a427df6f42507415836
sobte <i@sobte.me>
2025-04-28 12:07:19 +0800

    fix: remove irrelevant info

--- modules/home/nixos/desktop/xdg/default.nix
+++ modules/home/nixos/desktop/xdg/default.nix
@@ -24,7 +24,6 @@ in
       browser = mkOption {
         type = listOf str;
         default = [
-          "cfg.app.browser;"
           "chromium.desktop"
           "google-chrome.desktop"
         ];
