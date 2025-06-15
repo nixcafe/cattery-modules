8484dff7a7f0139692802f95155a4b3110b1dafc
sobte <i@sobte.me>
2025-06-14 20:18:35 -0400

    feat: remove yubikey-personalization-gui

--- modules/nixos/apps/yubikey/default.nix
+++ modules/nixos/apps/yubikey/default.nix
@@ -19,7 +19,6 @@ in
     environment.systemPackages = with pkgs; [
       yubikey-manager
       yubikey-personalization
-      yubikey-personalization-gui
       yubico-piv-tool
       yubioath-flutter
     ];
