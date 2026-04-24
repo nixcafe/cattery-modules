5960751486fd6c4fc2daa001e4e5f9d097b2ca69
sobte <i@sobte.me>
2026-04-25 02:52:35 +0800

    feat: use systemd init impermance

--- lib/utils/default.nix
+++ lib/utils/default.nix
@@ -4,7 +4,14 @@ let
     splitString
     sublist
     concatStringsSep
+    removePrefix
+    removeSuffix
+    hasPrefix
+    stringToCharacters
+    replaceStrings
     ;
+
+  inherit (lib.strings) escapeC normalizePath;
 in
 {
   findNixPaths =
@@ -23,4 +30,26 @@ in
       root = sublist (length - 2) (length - 1) names;
     in
     concatStringsSep "." root;
+
+  # see: https://github.com/NixOS/nixpkgs/blob/master/nixos/lib/utils.nix
+  # Escape a path according to the systemd rules. FIXME: slow
+  # The rules are described in systemd.unit(5) as follows:
+  # The escaping algorithm operates as follows: given a string, any "/" character is replaced by "-", and all other characters which are not ASCII alphanumerics, ":", "_" or "." are replaced by C-style "\x2d" escapes. In addition, "." is replaced with such a C-style escape when it would appear as the first character in the escaped string.
+  # When the input qualifies as absolute file system path, this algorithm is extended slightly: the path to the root directory "/" is encoded as single dash "-". In addition, any leading, trailing or duplicate "/" characters are removed from the string before transformation. Example: /foo//bar/baz/ becomes "foo-bar-baz".
+  escapeSystemdPath =
+    s:
+    let
+      replacePrefix =
+        p: r: s:
+        (if (hasPrefix p s) then r + (removePrefix p s) else s);
+      trim = s: removeSuffix "/" (removePrefix "/" s);
+      normalizedPath = normalizePath s;
+    in
+    replaceStrings [ "/" ] [ "-" ] (
+      replacePrefix "." (escapeC [ "." ] ".") (
+        escapeC (stringToCharacters " !\"#$%&'()*+,;<=>=@[\\]^`{|}~-") (
+          if normalizedPath == "/" then normalizedPath else trim normalizedPath
+        )
+      )
+    );
 }
--- modules/nixos/system/fileSystems/btrfs/impermanence/default.nix
+++ modules/nixos/system/fileSystems/btrfs/impermanence/default.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   config,
   lib,
   namespace,
@@ -6,8 +7,11 @@
 }:
 let
   inherit (lib) mkOption types;
+  inherit (lib.${namespace}) escapeSystemdPath;
 
   cfg = config.${namespace}.system.fileSystems.btrfs.impermanence;
+
+  deviceUnits = map (x: "${escapeSystemdPath x.device}.device") (builtins.attrValues cfg);
 in
 {
   options.${namespace}.system.fileSystems.btrfs.impermanence = mkOption {
@@ -39,35 +43,64 @@ in
   };
 
   config = {
-    boot.initrd.postDeviceCommands = lib.mkAfter (
-      builtins.concatStringsSep "\n" (
-        map (x: ''
-          mkdir ${x.tempDir}
-          mount ${x.device} ${x.tempDir}
-          if [[ -e ${x.tempDir}/${x.subvol} ]]; then
-            mkdir -p ${x.tempDir}/${x.oldSubvolDir}
-            timestamp=$(date --date="@$(stat -c %Y ${x.tempDir}/${x.subvol})" "+%Y-%m-%-d_%H:%M:%S")
-            btrfs subvolume snapshot ${x.tempDir}/${x.subvol} "${x.tempDir}/${x.oldSubvolDir}/$timestamp"
-            btrfs subvolume delete ${x.tempDir}/${x.subvol}
-          fi
+    boot.initrd.systemd = {
+      services.impermance-btrfs-rolling = {
+        description = "Archiving existing BTRFS subvolume and creating a fresh one";
+        # Specify dependencies explicitly
+        unitConfig.DefaultDependencies = false;
+        # The script needs to run to completion before this service is done
+        serviceConfig = {
+          Type = "oneshot";
+          # NOTE: to be able to see errors in your script do this
+          StandardOutput = "journal+console";
+          StandardError = "journal+console";
+        };
+        # This service is required for boot to succeed
+        requiredBy = [ "initrd.target" ];
+        # Should complete before any file systems are mounted
+        before = [ "sysroot.mount" ];
 
-          delete_subvolume_recursively() {
-            IFS=$'\n'
-            for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-              delete_subvolume_recursively "${x.tempDir}/$i"
+        requires = [
+          "initrd-root-device.target"
+        ]
+        ++ deviceUnits;
+
+        after = [
+          "initrd-root-device.target"
+          # Allow hibernation to resume before trying to alter any data
+          "local-fs-pre.target"
+        ]
+        ++ deviceUnits;
+
+        script = builtins.concatStringsSep "\n" (
+          map (x: ''
+            mkdir ${x.tempDir}
+            mount -t btrfs -o user_subvol_rm_allowed ${x.device} ${x.tempDir}
+            if [[ -e ${x.tempDir}/${x.subvol} ]]; then
+              mkdir -p ${x.tempDir}/${x.oldSubvolDir}
+              timestamp=$(date --date="@$(stat -c %Y ${x.tempDir}/${x.subvol})" "+%Y-%m-%-d_%H:%M:%S")
+              btrfs subvolume snapshot ${x.tempDir}/${x.subvol} "${x.tempDir}/${x.oldSubvolDir}/$timestamp"
+              btrfs subvolume delete ${x.tempDir}/${x.subvol}
+            fi
+
+            for old_subvol in $(find ${x.tempDir}/${x.oldSubvolDir}/ -maxdepth 1 -mtime +30); do
+              btrfs subvolume delete -R "$old_subvol"
             done
-            btrfs subvolume delete "$1"
-          }
 
-          for i in $(find ${x.tempDir}/${x.oldSubvolDir}/ -maxdepth 1 -mtime +30); do
-            delete_subvolume_recursively "$i"
-          done
+            btrfs subvolume create ${x.tempDir}/${x.subvol}
+            umount ${x.tempDir}
+          '') (builtins.attrValues cfg)
+        );
+      };
 
-          btrfs subvolume create ${x.tempDir}/${x.subvol}
-          umount ${x.tempDir}
-        '') (builtins.attrValues cfg)
-      )
-    );
+      extraBin = {
+        "mkdir" = "${pkgs.coreutils}/bin/mkdir";
+        "date" = "${pkgs.coreutils}/bin/date";
+        "stat" = "${pkgs.coreutils}/bin/stat";
+        "find" = "${pkgs.findutils}/bin/find";
+        "btrfs" = "${pkgs.btrfs-progs}/bin/btrfs";
+      };
+    };
   };
 
 }
